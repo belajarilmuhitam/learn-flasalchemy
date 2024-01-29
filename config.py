e36283d0652fbf17211dd7d8683187702c18ce91
Chandra Halim <chan.halim98@gmail.com>
2024-01-29 05:30:44 -0800

    f

--- config.py
+++ config.py
@@ -3,11 +3,12 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    HOST = str(os.environ.get("DB_HOST"))
-    DATABASE = str(os.environ.get("DB_DATABASE"))
-    USERNAME = str(os.environ.get("DB_USERNAME"))
-    PASSWORD = str(os.environ.get("DB_PASSWORD"))
+    HOST = os.environ.get("DB_HOST")
+    PORT = os.environ.get("DB_PORT", "3306")
+    DATABASE = os.environ.get("DB_DATABASE")
+    USERNAME = os.environ.get("DB_USERNAME")
+    PASSWORD = os.environ.get("DB_PASSWORD")
 
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymsql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DATABASE
-    SQLALCHEMY_TRACK_MODIFICATION = False
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}?charset=utf8mb4"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
